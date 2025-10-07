6ddc1ac251169ac08afe05dcee7317fc6c94dc46
Mursal Ashraf <mash0019@student.monash.edu>
2025-10-07 22:32:36 +1100

    [master] (Mursal) update vite config

--- codebase/frontend/vite.config.ts
+++ codebase/frontend/vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
